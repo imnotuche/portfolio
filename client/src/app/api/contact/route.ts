81e513b3e8fcd8845342ad0114da22096d19679c
Uche <uchechukwuekwe714@gmail.com>
2026-05-29 14:49:00 +0100

    Fixed env variable in api/contact route file

--- client/src/app/api/contact/route.ts
+++ client/src/app/api/contact/route.ts
@@ -11,8 +11,8 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_APP_PASSWORD,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
         },
     });
 
